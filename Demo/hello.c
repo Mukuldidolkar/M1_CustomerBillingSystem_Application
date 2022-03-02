9a4a439152e6d54b4258ad73f9870a801ed215ff
Mukul Didolkar <98812378+Mukuldidolkar@users.noreply.github.com>
2022-03-02 15:42:31 +0530

    Update hello.c

--- Demo/hello.c
+++ Demo/hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main()
 {
-    printf("Welcome to Github Actions.\n********* ItsWorking*******\n");
+    printf("Welcome to Github Actions.\n********* It is Working fine*******\n");
     return 0;
 }
